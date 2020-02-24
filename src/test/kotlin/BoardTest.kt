259d3ffd65c73eb617866f7750ba94d31cc49c10
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-24 23:41:13 +0000

    add row and column tests

--- src/test/kotlin/BoardTest.kt
+++ src/test/kotlin/BoardTest.kt
@@ -1,63 +1,49 @@
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.lang.IllegalArgumentException
+import java.util.stream.Stream
 
+@DisplayName("Test board data structure")
 class BoardTest {
 
-    @DisplayName("Test functions on lists")
-    @Nested
-    inner class UtilFunctions {
+    companion object {
+        @JvmStatic
+        fun boards(): Stream<Board> = (1..4).map { squareBoard(it) }.stream()
+    }
 
-        @Test
-        internal fun canTakeHeadOfNoneEmptyList() {
-            assertThat(listOf(1,2,3,4,5).head(), `is`(1))
+    // fun boards(): Stream<Board> = (1..4).map { squareBoard(it) }.stream()
 
-            assertThat(listOf(1,2).head(), `is`(1))
-        }
+    // @ParameterizedTest
+    // @MethodSource("boards")
+    @Test
+    internal fun canGetRowsFromWellFormedBoard() {
 
-        @Test
-        internal fun headOfEmptyListThrowsException() {
-            assertThrows<NoSuchElementException> { emptyList<Any>().head() }
-        }
+        assertThat(rows(squareBoard(1)), `is`(listOf(listOf(0))))
 
-        @Test
-        internal fun canTakeTailOfNoneEmptyList() {
-            assertThat(listOf(1,2,3,4,5).tail(), `is`(listOf(2,3,4,5)))
+        assertThat(rows(squareBoard(2)), `is`(listOf(listOf(0,1), listOf(2,3))))
 
-            assertThat(listOf(1,2).tail(), `is`(listOf(2)))
-        }
+        assertThat(rows(squareBoard(3)), `is`(listOf(listOf(0,1,2), listOf(3,4,5), listOf(6,7,8))))
+    }
 
-        @Test
-        internal fun tailOfEmptyListIsEmptyList() {
-            assertThat(emptyList<Any>().tail(), `is`(emptyList()))
-        }
+    @Test
+    internal fun rowsFromMalformedBoardThrowsException() {
+        assertThrows<IllegalArgumentException> { rows(squareBoard(0)) }
 
-        @Test
-        internal fun canAppendAnyElementToAList() {
-            assertThat(1 append listOf(2,3,4,5), `is`(listOf(1,2,3,4,5)))
+        assertThrows<IllegalArgumentException> { rows(squareBoard(-1)) }
+    }
 
-            assertThat('a' append listOf(2,3,4,5), `is`(listOf('a', 2,3,4,5)))
-        }
+    @Test
+    internal fun canGetColumnsFromWellFormedBoard() {
 
-        @Test
-        internal fun canTransposeListOfListOfAnyLength() {
-            assertThat(listOf(listOf(1,2), listOf(3,4)).transpose(),
-                `is`(listOf(listOf(1,3), listOf(2,4))))
+        assertThat(columns(squareBoard(1)), `is`(listOf(listOf(0))))
 
-            assertThat(listOf(listOf(1,2), listOf(3)).transpose(),
-                `is`(listOf(listOf(1,3), listOf(2))))
+        assertThat(columns(squareBoard(2)), `is`(listOf(listOf(0,2), listOf(1,3))))
 
-            assertThat(listOf(listOf(1,2)).transpose(),
-                `is`(listOf(listOf(1), listOf(2))))
+        assertThat(columns(squareBoard(3)), `is`(listOf(listOf(0,3,6), listOf(1,4,7), listOf(2,5,8))))
 
-            assertThat(listOf(listOf(1,2,3), listOf(4,5)).transpose(),
-                `is`(listOf(listOf(1,4), listOf(2,5), listOf(3))))
-
-            assertThat(listOf(listOf(1,2), listOf(), listOf(4)).transpose(),
-                `is`(listOf(listOf(1,4), listOf(2))))
-        }
     }
 }
