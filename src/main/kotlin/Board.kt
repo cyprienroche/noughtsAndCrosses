d3ef538c51526cc8134541a850893e8fd4203e23
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-25 23:24:52 +0000

    add Cell data class and use List of List of Cell in Board

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -1,10 +1,12 @@
 
-data class Board(val cells: List<Int>, val dim: Int)
+data class Board(val cells: List<Cell>, val dim: Int)
 
-fun squareBoard(dim: Int): Board = Board(cells = (0 until (dim * dim)).toList(), dim = dim)
+fun squareBoard(dim: Int): Board = Board(cells = (0 until (dim * dim)).map { Cell(it) }.toList(), dim = dim)
 
-fun rows(board: Board): List<List<Int>> = board.cells.chunked(board.dim)
+fun rows(board: Board): List<List<Cell>> = board.cells.chunked(board.dim)
 
-fun columns(board: Board): List<List<Int>> = rows(board).transpose()
+fun columns(board: Board): List<List<Cell>> = rows(board).transpose()
 
-fun diagonals(board: Board): List<List<Int>> = diagIndexes(board.dim).map { indexes -> indexes.map { board.cells[it] } }
+fun diagonals(board: Board): List<List<Cell>> = diagIndexes(board.dim).map { indexes -> indexes.map { board.cells[it] } }
+
+data class Cell(val id: Int)
--- src/test/kotlin/BoardTest.kt
+++ src/test/kotlin/BoardTest.kt
@@ -1,6 +1,5 @@
 
 import java.lang.IllegalArgumentException
-import java.util.stream.Stream
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.DisplayName
@@ -10,22 +9,15 @@ import org.junit.jupiter.api.assertThrows
 @DisplayName("Test board data structure")
 class BoardTest {
 
-    companion object {
-        @JvmStatic
-        fun boards(): Stream<Board> = (1..4).map { squareBoard(it) }.stream()
-    }
+    private fun List<List<Int>>.idToCells(): List<List<Cell>> = this.map { it.map { id -> Cell(id) } }
 
-    // fun boards(): Stream<Board> = (1..4).map { squareBoard(it) }.stream()
-
-    // @ParameterizedTest
-    // @MethodSource("boards")
     @Test
     internal fun canGetRowsFromSquaredBoard() {
-        assertThat(rows(squareBoard(1)), `is`(listOf(listOf(0))))
+        assertThat(rows(squareBoard(1)), `is`(listOf(listOf(0)).idToCells()))
 
-        assertThat(rows(squareBoard(2)), `is`(listOf(listOf(0, 1), listOf(2, 3))))
+        assertThat(rows(squareBoard(2)), `is`(listOf(listOf(0, 1), listOf(2, 3)).idToCells()))
 
-        assertThat(rows(squareBoard(3)), `is`(listOf(listOf(0, 1, 2), listOf(3, 4, 5), listOf(6, 7, 8))))
+        assertThat(rows(squareBoard(3)), `is`(listOf(listOf(0, 1, 2), listOf(3, 4, 5), listOf(6, 7, 8)).idToCells()))
     }
 
     @Test
@@ -37,11 +29,11 @@ class BoardTest {
 
     @Test
     internal fun canGetColumnsFromSquareBoard() {
-        assertThat(columns(squareBoard(1)), `is`(listOf(listOf(0))))
+        assertThat(columns(squareBoard(1)), `is`(listOf(listOf(0)).idToCells()))
 
-        assertThat(columns(squareBoard(2)), `is`(listOf(listOf(0, 2), listOf(1, 3))))
+        assertThat(columns(squareBoard(2)), `is`(listOf(listOf(0, 2), listOf(1, 3)).idToCells()))
 
-        assertThat(columns(squareBoard(3)), `is`(listOf(listOf(0, 3, 6), listOf(1, 4, 7), listOf(2, 5, 8))))
+        assertThat(columns(squareBoard(3)), `is`(listOf(listOf(0, 3, 6), listOf(1, 4, 7), listOf(2, 5, 8)).idToCells()))
     }
 
     @Test
@@ -53,11 +45,11 @@ class BoardTest {
 
     @Test
     internal fun canGetDiagonalFromWellFormedBoard() {
-        assertThat(diagonals(squareBoard(1)), `is`(listOf(listOf(0), listOf(0))))
+        assertThat(diagonals(squareBoard(1)), `is`(listOf(listOf(0), listOf(0)).idToCells()))
 
-        assertThat(diagonals(squareBoard(2)), `is`(listOf(listOf(0, 3), listOf(1, 2))))
+        assertThat(diagonals(squareBoard(2)), `is`(listOf(listOf(0, 3), listOf(1, 2)).idToCells()))
 
-        assertThat(diagonals(squareBoard(3)), `is`(listOf(listOf(0, 4, 8), listOf(2, 4, 6))))
+        assertThat(diagonals(squareBoard(3)), `is`(listOf(listOf(0, 4, 8), listOf(2, 4, 6)).idToCells()))
     }
 
     @Test
