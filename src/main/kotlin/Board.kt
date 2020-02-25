733281a127fa8af83d2f1a1430b653d69bc9dc8c
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-25 22:13:45 +0000

    move diagIndex* fun to utils.kt and change order of fun in utils.kt

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -8,9 +8,3 @@ fun rows(board: Board): List<List<Int>> = board.cells.chunked(board.dim)
 fun columns(board: Board): List<List<Int>> = rows(board).transpose()
 
 fun diagonals(board: Board): List<List<Int>> = diagIndexes(board.dim).map { indexes -> indexes.map { board.cells[it] } }
-
-private fun diagIndexes(n: Int) = listOf(diagIndexSE(n), diagIndexSW(n))
-
-private fun diagIndexSW(n: Int) = (1..n).map { it * (n - 1) }
-
-private fun diagIndexSE(n: Int) = (0..n - 1).map { it * (n + 1) }
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,11 +1,17 @@
 
-fun <E> List<List<E>>.transpose(): List<List<E>> = when {
-    this.first().isEmpty() -> emptyList()
-    else -> this.filter { it.isNotEmpty() }.map { it.head() } append this.map { it.tail() }.transpose()
-}
-
 fun <E> List<E>.head(): E = first()
 
 fun <E> List<E>.tail(): List<E> = drop(1)
 
 infix fun <E> E.append(xs: List<E>): List<E> = listOf(this).plus(xs)
+
+fun <E> List<List<E>>.transpose(): List<List<E>> = when {
+    this.first().isEmpty() -> emptyList()
+    else -> this.filter { it.isNotEmpty() }.map { it.head() } append this.map { it.tail() }.transpose()
+}
+
+fun diagIndexSW(n: Int) = (1..n).map { it * (n - 1) }
+
+fun diagIndexSE(n: Int) = (0..n - 1).map { it * (n + 1) }
+
+fun diagIndexes(n: Int) = listOf(diagIndexSE(n), diagIndexSW(n))
