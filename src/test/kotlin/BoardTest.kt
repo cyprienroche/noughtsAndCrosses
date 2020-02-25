24237e02331287addebe36c5bc8d08752e75ba85
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-25 22:10:07 +0000

    reformat code using ktlint

--- src/test/kotlin/BoardTest.kt
+++ src/test/kotlin/BoardTest.kt
@@ -1,11 +1,11 @@
 
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.`is`
-import org.junit.jupiter.api.assertThrows
 import java.lang.IllegalArgumentException
 import java.util.stream.Stream
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 @DisplayName("Test board data structure")
 class BoardTest {
@@ -21,12 +21,11 @@ class BoardTest {
     // @MethodSource("boards")
     @Test
     internal fun canGetRowsFromSquaredBoard() {
-
         assertThat(rows(squareBoard(1)), `is`(listOf(listOf(0))))
 
-        assertThat(rows(squareBoard(2)), `is`(listOf(listOf(0,1), listOf(2,3))))
+        assertThat(rows(squareBoard(2)), `is`(listOf(listOf(0, 1), listOf(2, 3))))
 
-        assertThat(rows(squareBoard(3)), `is`(listOf(listOf(0,1,2), listOf(3,4,5), listOf(6,7,8))))
+        assertThat(rows(squareBoard(3)), `is`(listOf(listOf(0, 1, 2), listOf(3, 4, 5), listOf(6, 7, 8))))
     }
 
     @Test
@@ -38,13 +37,11 @@ class BoardTest {
 
     @Test
     internal fun canGetColumnsFromSquareBoard() {
-
         assertThat(columns(squareBoard(1)), `is`(listOf(listOf(0))))
 
-        assertThat(columns(squareBoard(2)), `is`(listOf(listOf(0,2), listOf(1,3))))
+        assertThat(columns(squareBoard(2)), `is`(listOf(listOf(0, 2), listOf(1, 3))))
 
-        assertThat(columns(squareBoard(3)), `is`(listOf(listOf(0,3,6), listOf(1,4,7), listOf(2,5,8))))
-
+        assertThat(columns(squareBoard(3)), `is`(listOf(listOf(0, 3, 6), listOf(1, 4, 7), listOf(2, 5, 8))))
     }
 
     @Test
@@ -58,10 +55,9 @@ class BoardTest {
     internal fun canGetDiagonalFromWellFormedBoard() {
         assertThat(diagonals(squareBoard(1)), `is`(listOf(listOf(0), listOf(0))))
 
-        assertThat(diagonals(squareBoard(2)), `is`(listOf(listOf(0,3), listOf(1,2))))
+        assertThat(diagonals(squareBoard(2)), `is`(listOf(listOf(0, 3), listOf(1, 2))))
 
-        assertThat(diagonals(squareBoard(3)), `is`(listOf(listOf(0,4,8), listOf(2,4,6))))
-
+        assertThat(diagonals(squareBoard(3)), `is`(listOf(listOf(0, 4, 8), listOf(2, 4, 6))))
     }
 
     @Test
@@ -69,6 +65,5 @@ class BoardTest {
         assertThat(diagonals(squareBoard(0)), `is`(listOf(listOf(), listOf())))
 
         assertThat(diagonals(squareBoard(-1)), `is`(listOf(listOf(), listOf())))
-
     }
 }
