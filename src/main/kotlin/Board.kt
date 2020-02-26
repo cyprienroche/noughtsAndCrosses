ca675a3ec4d480ad83dcc821f7472614cbe6af3b
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-26 00:00:08 +0000

    add replace fun so can replace a cell with that of a player

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -1,5 +1,11 @@
 
-data class Board(val cells: List<Cell>, val dim: Int)
+data class Board(var cells: List<Cell>, val dim: Int) {
+    fun replace(n: Int, elem: Cell) {
+        cells = replace(n, elem, cells)
+    }
+
+    override fun toString(): String = rows(this).fold("", { acc, row -> acc + row.fold("", { acc, cell -> "$acc$cell " }) + "\n" })
+}
 
 fun squareBoard(dim: Int): Board = Board(cells = (0 until (dim * dim)).map { Empty(it) }.toList(), dim = dim)
 
@@ -9,10 +15,17 @@ fun columns(board: Board): List<List<Cell>> = rows(board).transpose()
 
 fun diagonals(board: Board): List<List<Cell>> = diagIndexes(board.dim).map { indexes -> indexes.map { board.cells[it] } }
 
-sealed class Cell(val pos: Int)
-data class Empty(val p: Int): Cell(p)
-data class Taken(val p: Int, val player: Player): Cell(p)
-
-enum class Player {
-    X, O
+fun <E> replace(n: Int, elem: E, list: List<E>): List<E> = when(n) {
+    0 -> elem cons list.tail()
+    else -> list.head() cons replace(n - 1, elem, list.tail())
 }
+
+sealed class Cell(val pos: Int)
+data class Empty(val p: Int): Cell(p) {
+    override fun toString(): String = ""
+}
+data class Taken(val p: Int, val player: Player): Cell(p) {
+    override fun toString(): String = "$player"
+}
+
+enum class Player { X, O }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,8 @@
 
 fun main() {
+    val board = squareBoard(2)
+    board.replace(0, Taken(0, Player.X))
+    board.replace(1, Taken(1, Player.O))
+    board.replace(2, Taken(2, Player.X))
+    println(board)
 }
