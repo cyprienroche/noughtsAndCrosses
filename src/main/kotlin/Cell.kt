6d1c5df9b7ba30a53befe52e073a92c2969b4382
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-26 21:11:28 +0000

    move Cell Player and Position to own file

---
+++ src/main/kotlin/Cell.kt
@@ -0,0 +1,9 @@
+sealed class Cell
+
+object Empty : Cell() {
+    override fun toString(): String = ""
+}
+
+data class Taken(val player: Player) : Cell() {
+    override fun toString(): String = "$player"
+}
---
+++ src/main/kotlin/Player.kt
@@ -0,0 +1,1 @@
+enum class Player { X, O }
