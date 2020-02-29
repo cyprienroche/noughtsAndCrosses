994d48678ef71b253546ef4164476b791f5c3425
cr618 <cyprien.roche18@imperial.ac.uk>
2020-02-29 16:41:56 +0000

    refactor spacing in boardTest

--- src/test/kotlin/BoardTest.kt
+++ src/test/kotlin/BoardTest.kt
@@ -9,8 +9,12 @@ class BoardTest {
 
     private val board1WinnerX: Board = SquareBoard(listOf(Taken(Player.X)))
     private val board1NoWinner: Board = SquareBoard(2)
+
     private val board2WinnerX: Board = SquareBoard(listOf(Taken(Player.X), Taken(Player.X), Taken(Player.O), Empty))
     private val board2NoWinner: Board = SquareBoard(listOf(Taken(Player.X), Taken(Player.O), Empty, Empty))
+
+    private val board3NoWinner: Board = SquareBoard(3)
+    private val board3WinnerO: Board = SquareBoard(listOf(Taken(Player.O), Taken(Player.X), Empty, Empty, Taken(Player.O), Taken(Player.X), Empty, Empty, Taken(Player.O)))
 
     @Test
     internal fun correctlyIdentifyWinner1By1() {
@@ -22,5 +26,11 @@ class BoardTest {
     internal fun correctlyIdentifyWinner2By2() {
         assertThat(board2WinnerX.winner(), `is`(Player.X))
         assertNull(board2NoWinner.winner())
+    }
+
+    @Test
+    internal fun correctlyIdentifyWinner3By3() {
+        assertThat(board3WinnerO.winner(), `is`(Player.O))
+        assertNull(board3NoWinner.winner())
     }
 }
